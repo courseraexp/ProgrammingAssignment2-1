dcdb5612c77d193575e9d5fafdda753c149cf680
courseraexp <courseraexp@users.noreply.github.com>
2015-02-16 23:32:13 -0500

    Update and rename MakeVector.R to CacheSolve.R

---
+++ CacheSolve.R
@@ -0,0 +1,13 @@
+CacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+}
+
+
